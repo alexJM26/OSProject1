0b781686abc11e15acdf098dc21b7411f856e516
alexJM26 <85090641+alexJM26@users.noreply.github.com>
2024-04-04 11:56:07 -0400

    Update sharedMemory.hpp

--- sharedMemory.hpp
+++ sharedMemory.hpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <thread>           // threads
+#include <semaphore.h>      // sem_t methods
+#include <sys/mman.h>       // shared memory object
+#include <sys/stat.h>       // mode constants 
+#include <fcntl.h>          // O_* constants 
+#include <unistd.h>         // ftruncate
 
+#define table_size 2 // table can only hold two items at once
+
+struct sharedMemory {
+    char table[table_size]; // table to hold items
+    int in = 0, out = 0; // indicate next element in table to be produced/consumed
+
+    sem_t available; // indicates if one process is in its critical section
+    sem_t full, empty; // become zero when table is full/empty
+};
