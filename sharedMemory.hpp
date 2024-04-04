56651b7ea6e7dbe1b496f62486b7885533d427f5
alexJM26 <85090641+alexJM26@users.noreply.github.com>
2024-04-04 14:50:27 -0400

    Update sharedMemory.hpp

--- sharedMemory.hpp
+++ sharedMemory.hpp
@@ -7,11 +7,12 @@
 #include <unistd.h>         // ftruncate
 
 #define table_size 2 // table can only hold two items at once
+#define size 5 // amount of items produced / consumed
 
 struct sharedMemory {
     char table[table_size]; // table to hold items
     int in = 0, out = 0; // indicate next element in table to be produced/consumed
 
-    sem_t available; // indicates critical section
-    sem_t full, empty; // indicates if table is full/empty
+    sem_t available; // indicates critical section in use
+    sem_t full, empty; // indicates when table is full/empty
 };
